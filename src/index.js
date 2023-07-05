6f194b9ac3ba88e7405b21583f0952b10c81464a
sheenamorjaria <135012013+sheenamorjaria@users.noreply.github.com>
2023-07-05 14:49:43 +0100

    injected html into js
    moved forecast to js from html

--- src/index.js
+++ src/index.js
@@ -24,6 +24,32 @@ if (hours < 10) {
   hours = `0${hours}`;
 }
 
+function displayForecast() {
+  let forecast = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="col-2">
+    <div class="forecast-date"><h5>${day} </h5></div>
+    <img
+     src="images/suncloud.jpg"
+     alt="suncloud"
+     width="40"
+    />
+    <div class="forecat-temperature"> <h6>
+    <span class="forecast-temperature-max"> 14° </span> 
+    <span class="forecast-temperature-max"> 7° </span> </h6>
+    </div>
+  </div>
+  `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecast.innerHTML = forecastHTML;
+}
+
 let h2 = document.querySelector("h2");
 h2.innerHTML = `Last updated: ${day}, ${hours}:${minutes}`;
 
@@ -50,7 +76,10 @@ function showLocation(position) {
 function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let currentTemp = document.querySelector("#current-temp");
-  currentTemp.innerHTML = ` ${temperature}`;
+  currentTemp.innerHTML = `${temperature}`;
+
+  celsiusTemperature = response.data.main.temp;
+
   let cityName = response.data.name;
   let country = response.data.sys.country;
   let h1 = document.querySelector("h1");
@@ -81,39 +110,13 @@ function search(city) {
   let apiKey = "8ca7dd4e61360b90fb66918853670e48";
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showWeather);
+  axios.get(apiUrl).then(showTemperature);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#entercity").value;
   search(city);
-}
-
-function showWeather(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let currentTemp = document.querySelector("#current-temp");
-  currentTemp.innerHTML = ` ${temperature}`;
-
-  celsiusTemperature = response.data.main.temp;
-
-  let cityName = response.data.name;
-  let country = response.data.sys.country;
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `${cityName}, ${country}`;
-  let wind = document.querySelector("#wind");
-  wind.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} mph`;
-  let humid = document.querySelector("#humidity");
-  humid.innerHTML = `Humidity: ${Math.round(response.data.main.humidity)}%`;
-  let description = response.data.weather[0].main;
-  let currentdescription = document.querySelector("#description");
-  currentdescription.innerHTML = `${description}`;
-  let currentImage = document.querySelector("#currentweather-image");
-  currentImage.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  currentImage.setAttribute("alt", response.data.weather[0].description);
 }
 
 function showFahrenheit(event) {
@@ -145,3 +148,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsius);
 
 search("London");
+
+displayForecast();
